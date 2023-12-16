2f7936f0f53bb4f44dfefdd17faceb2fff5beaba
yasir meraj <yasir.meraj@gmail.com>
2023-12-17 02:07:46 +0500

    Completed Assignment02 Calculator PIAC Batch 51

---
+++ calculator.ts
@@ -0,0 +1,25 @@
+function sum(num1:number,num2:number):number {
+
+    return (num1+num2);
+
+}
+
+function subtract(num1:number,num2:number):number {
+
+    return (num1-num2);
+
+}
+
+function multiply(num1:number,num2:number):number {
+
+    return (num1*num2);
+
+}
+
+function divide(num1:number,num2:number):number {
+
+    return (num1/num2);
+
+}
+
+export {sum,subtract,multiply,divide};
---
+++ main.ts
@@ -0,0 +1,73 @@
+import inquirer from "inquirer";
+import {sum,subtract,multiply,divide} from "./calculator.js"
+
+let loop:boolean=true;
+while (loop) {
+        let v_answers = await inquirer.prompt(
+            
+        [
+
+            //Take Input for number01
+            {
+                type:"number",
+                name:"num1",
+                message:"Enter first number:"
+            },
+
+            //Take Input for number02
+            {
+
+                type:"number",
+                name:"num2",
+                message:"Enter second number:"
+
+
+            },
+            {
+
+                type:"list",
+                name:"operator",
+                message:"Select Arithmetic Operation:",
+                choices:["+","-","*","/","e"]
+
+            }
+
+        ]   
+        );
+
+       //Check Addition Operator is Selected
+        if(v_answers.operator==="+") {
+
+            console.log(`${v_answers.num1} + ${v_answers.num2} = `
+            +sum(v_answers.num1,v_answers.num2));
+
+        }
+        //Check Subtraction Operator is Selected
+        else if (v_answers.operator==="-") {
+
+            console.log(`${v_answers.num1} - ${v_answers.num2} = `
+            +subtract(v_answers.num1,v_answers.num2));
+
+        }
+        //Check Multiplication Operator is Selected
+        else if (v_answers.operator==="*") {
+
+            console.log(`${v_answers.num1} * ${v_answers.num2} = `
+            +multiply(v_answers.num1,v_answers.num2));
+
+
+        }
+        //Check Division Operator is Selected
+        else if (v_answers.operator==="/") {
+
+            console.log(`${v_answers.num1} / ${v_answers.num2} = `
+            +divide(v_answers.num1,v_answers.num2));
+
+        }
+        else if (v_answers.operator==="e") {
+
+            break;
+        }
+
+
+} //end of while loop
