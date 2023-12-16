e44d627a08acab95b1d83ceebe63e922990ced58
yasirmeraj <71040430+yasirmeraj@users.noreply.github.com>
2023-12-17 02:31:05 +0500

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,13 @@
+function sum(num1, num2) {
+    return (num1 + num2);
+}
+function subtract(num1, num2) {
+    return (num1 - num2);
+}
+function multiply(num1, num2) {
+    return (num1 * num2);
+}
+function divide(num1, num2) {
+    return (num1 / num2);
+}
+export { sum, subtract, multiply, divide };
